c8718521a5c430e46a994dc20921116ebc250c21
infodelta18-creator <infodelta18@gmail.com>
2026-02-13 22:00:31 +0500

    Update not-found.tsx

--- src/app/[category]/not-found.tsx
+++ src/app/[category]/not-found.tsx
@@ -16,7 +16,7 @@ export default function NotFound() {
               href="/"
               className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
             >
-              Go back home
+              Boshiga qaytish
               <svg
                 className="ml-2 -mr-1 w-5 h-5"
                 fill="none"
